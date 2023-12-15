aa1a029a4e13db468c6fb332d3842e37cad3965d
OE.omergunr100 <omer.er10@gmail.com>
2023-12-15 14:04:02 +0200

    Add project files.

---
+++ FlowermanISeeYou/Patches/FlowermanAIPatch.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FlowermanISeeYou.Patches
+{
+    [HarmonyPatch(typeof(FlowermanAI))]
+    internal class FlowermanAIPatch
+    {
+        internal static AudioClip flowermanISeeYouSound = null;
+        internal static Dictionary<FlowermanAI, int> timeTriggeredSound = new Dictionary<FlowermanAI, int>();
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        private static void StartPatch(FlowermanAI __instance)
+        {
+            if (flowermanISeeYouSound == null)
+            {
+                AssetBundle ab = AssetBundle.LoadFromFile(Plugin.persistentDataPath);
+                ab.LoadAsset<AudioClip>("FlowermanISeeYouSound.mp3");
+            }
+        }
+    }
+}
---
+++ FlowermanISeeYou/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowermanISeeYou
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private const string GUID = "oe.tweaks.sounds.flowerman";
+        private const string NAME = "Flowerman I See You";
+        private const string VERSION = "1.0.0";
+
+        internal static Plugin instance;
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+        internal static string persistentDataPath = Paths.PluginPath + "\\OE.Tweaks\\Assets";
+
+        private void Awake()
+        {
+            log = this.Logger;
+            log.LogInfo("'Flowerman I See You' is loading...");
+
+            if (instance == null)
+                instance = this;
+
+            harmony.PatchAll();
+
+            log.LogInfo("'Flowerman I See You' loaded!");
+        }
+    }
+}
